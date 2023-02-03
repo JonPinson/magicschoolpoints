a2637a382bdead5fa0441d1e78ece628eefa0a10
Your Name <you@example.com>
2023-02-03 18:50:46 -0500

    initial

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>I did this for magic school points!</Text>
       <StatusBar style="auto" />
     </View>
   );
